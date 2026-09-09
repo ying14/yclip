231f84fe08fb75a279dd8d9e13b1ebb432e96004
Ying Taur <yingtaur@gmail.com>
2026-09-09 08:56:32 -0400

    reorganized claude contribution

--- data-raw/test.R
+++ data-raw/test.R
@@ -8,75 +8,45 @@ library(tidyverse)
 
 # pkgbuild::unload_dll("yclip")
 
-hello world
 clipboard_inspect()
-clipboard_inspect() %>% copy.to.clipboard()
-getClipboardFormats()
-
 
 # clipboard_read_raw("Rich Text Format")
 
 rtf <- clipboard_read_text("Rich Text Format")
 rtf
-writeLines(rtf, "epic_table.rtf")
 
-rtf %>% copy.to.clipboard()
-
-# rtf  %>% copy.to.clipboard()
-
-library(yingtools2)
 
 show_shapes()
 
-rows <- parse_rtf_rows(rtf)
-rows %>% length()
+
+clip_formats_available()
+
+gts <- clip_read("html")
+
+yclip:::clipboard_read_raw(format_name = "Html")
+
+html <- clipboard_read_raw("Html")
+clipboard_read_raw("Html")
+
+
+read_clipboard_raw("Html")
+
+
+yclip:::clipboard_read_raw_windows("Html")
+yclip:::clipboard_read_raw_windows("html")
+html
+
+html %>% class
+
+
+clip_write(html,"html")
 
 
 
-tables <- parse_epic_tables(rows)
-
-tables
 
 
-rows <- parse_rtf_rows(rtf)
-rows
-tables <- parse_epic_tables(rows)
-tables
-
-
-tables
-length(rows)
-sapply(rows, length)
-
-tables <- parse_rtf_tables(rtf)
-
-
-tables
-
-
-length(tables)
-
-tables
-
-rows
-# I'd expect:
-
-purrr::iwalk(rows, \(x, i) {
-  cat("\n--- ROW", i, "---\n")
-  print(x)
-})
-
-stringr::str_extract_all(
-  rtf,
-  "\\\\cell\\b"
-)[[1]][1:20]
-
-stringr::str_extract_all(
-  rtf,
-  "\\\\cell\\b|\\\\cellx[0-9]+"
-)[[1]][1:20]
-
-
+clip_write(html,"text")
+# clip_write(html,"rtf")
 
 
 
